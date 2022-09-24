8045e0cfa6611e938e751b702d898ba0dc9e2cad
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 12:32:48 -0400

    done

--- main.dart
+++ main.dart
@@ -1,13 +1,19 @@
-import 'dart:math';
 import 'package:latlong2/latlong.dart';
 
 void main() {
   List<double> logitude = [23, 24, 25];
   List<double> Lantiide = [40, 41, 42];
+  List<double> Oldlogitude = [23, 24, 26];
+  List<double> OldLantiide = [40, 41, 42];
   print(find().distance(logitude, Lantiide));
+  print(find().speed(logitude, Lantiide));
+  var cool = find().areYouAhead(logitude, Lantiide, Oldlogitude, OldLantiide);
+  print(cool.ahead);
+  print(cool.are_you_ahead);
+
 }
 
-class data {
+class bro_this_return_a_class_use_ahead_and_are_you_ahead {
   late double ahead;
   late bool are_you_ahead;
 }
@@ -36,9 +42,12 @@ class find {
     return distance(logitude, Lantiide) / logitude.length.toDouble();
   }
 
-  data areYouAhead(List<double> logitude, List<double> Lantiide,
-      List<double> Oldlogitude, List<double> OldLantiide) {
-    var return_data = data();
+  bro_this_return_a_class_use_ahead_and_are_you_ahead areYouAhead(
+      List<double> logitude,
+      List<double> Lantiide,
+      List<double> Oldlogitude,
+      List<double> OldLantiide) {
+    var return_data = bro_this_return_a_class_use_ahead_and_are_you_ahead();
     double new_distance = distance(logitude, Lantiide);
     double ghost = distance(Oldlogitude, OldLantiide);
     return_data.are_you_ahead = new_distance > ghost;
@@ -54,7 +63,6 @@ class find {
         }
         break;
     }
-
     return return_data;
   }
 }
