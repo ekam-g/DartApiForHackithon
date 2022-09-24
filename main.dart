f404e83dbcc12e78c06d75cd3aea024dc19c740f
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 11:33:58 -0400

    pubspec and other stuff

--- main.dart
+++ main.dart
@@ -1,13 +1,20 @@
-void main() {
+import 'dart:math';
+import 'package:latlong2/latlong.dart';
 
-}
-
+void main() {}
 
 class find {
-  int distance(List a) {
+  int distance(List logitude, List Lantiide) {
+    var math = Distance();
+    int totalDistance = 0;
+    for (int where = 0; where > logitude.length; where++) {
+      final meter =
+          math(LatLng(52.518611, 13.408056), LatLng(51.519475, 7.46694444));
+    }
 
     return 0;
   }
+
   int speed(List a) {
     return 0;
   }
