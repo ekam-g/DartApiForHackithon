6aa1a4fd77ade778be970ea3510b57e50f3188f1
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 11:41:23 -0400

    pubspec and other stuff

--- main.dart
+++ main.dart
@@ -1,18 +1,23 @@
 import 'dart:math';
 import 'package:latlong2/latlong.dart';
 
-void main() {}
+void main() {
+  List<double> logitude = [23, 25, 29];
+  List<double> Lantiide = [23, 25, 29];
+  print(find().distance(logitude, Lantiide));
+}
 
 class find {
-  int distance(List logitude, List Lantiide) {
+  double distance(List<double> logitude, List<double> Lantiide) {
     var math = Distance();
-    int totalDistance = 0;
-    for (int where = 0; where > logitude.length; where++) {
-      final meter =
-          math(LatLng(52.518611, 13.408056), LatLng(51.519475, 7.46694444));
+    double totalDistance = 0;
+    for (int where = 0; where < logitude.length; where = where + 2) {
+      totalDistance =
+          math(LatLng(Lantiide[where],  logitude[where]), LatLng(51.519475, 7.46694444)) +
+              totalDistance;
     }
 
-    return 0;
+    return totalDistance;
   }
 
   int speed(List a) {
