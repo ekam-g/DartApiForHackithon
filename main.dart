9325cc267f29f765cb22945ed87c332f796f51db
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 12:25:26 -0400

    done

--- main.dart
+++ main.dart
@@ -7,6 +7,11 @@ void main() {
   print(find().distance(logitude, Lantiide));
 }
 
+class data {
+  late double ahead;
+  late bool are_you_ahead;
+}
+
 class find {
   double distance(List<double> logitude, List<double> Lantiide) {
     var math = Distance();
@@ -24,7 +29,32 @@ class find {
     return totalDistance;
   }
 
-  int speed(List a) {
-    return 0;
+  double speed(
+    List<double> logitude,
+    List<double> Lantiide,
+  ) {
+    return distance(logitude, Lantiide) / logitude.length.toDouble();
+  }
+
+  data areYouAhead(List<double> logitude, List<double> Lantiide,
+      List<double> Oldlogitude, List<double> OldLantiide) {
+    var return_data = data();
+    double new_distance = distance(logitude, Lantiide);
+    double ghost = distance(Oldlogitude, OldLantiide);
+    return_data.are_you_ahead = new_distance > ghost;
+    switch (return_data.are_you_ahead) {
+      case true:
+        {
+          return_data.ahead = new_distance - ghost;
+        }
+        break;
+      default:
+        {
+          return_data.ahead = ghost - new_distance;
+        }
+        break;
+    }
+
+    return return_data;
   }
 }
